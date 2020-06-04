99b4be5a69711c46c73b2f68b3cfe26900ca76b7
hongzhenjie <550115977@qq.com>
2020-06-04 17:53:59 +0800

    UI管理器

---
+++ UnityGameFrame/Assets/Scripts/UI/BasePanel.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// UI面板基类
+/// </summary>
+public class BasePanel : MonoBehaviour {
+    protected Dictionary<string, List<UIBehaviour>> controlDic = new Dictionary<string, List<UIBehaviour>>();
+
+	// Use this for initialization
+	protected virtual void Awake () {
+        FindChildControl<Button>();
+        FindChildControl<Image>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// 得到对应名字的对应控件脚本
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="controlName"></param>
+    /// <returns></returns>
+    protected T GetControl<T>(string controlName) where T : UIBehaviour
+    {
+        if (controlDic.ContainsKey(controlName))
+        {
+            for (int i = 0; i < controlDic[controlName].Count; ++i)
+            {
+                if (controlDic[controlName][i] is T)
+                    return controlDic[controlName][i] as T;
+            }
+        }
+
+        return null;
+    }
+
+    protected void FindChildControl<T>() where T:UIBehaviour
+    {
+        T[] controls = GetComponentsInChildren<T>();
+        string name;
+        for (int i = 0; i < controls.Length; i++)
+        {
+            T control = controls[i];
+            name = control.gameObject.name;
+            if (controlDic.ContainsKey(name))
+            {
+                controlDic[name].Add(control);
+            }else
+            {
+                controlDic.Add(name, new List<UIBehaviour>() { control });
+            }
+            //添加点击事件
+            if(control is Button)
+            {
+                (control as Button).onClick.AddListener(()=>
+                {
+                    OnClick(name);
+                });
+            }
+        }
+    }
+
+    protected virtual void OnClick(string name)
+    {
+
+    }
+
+    /// <summary>
+    /// 显示自己
+    /// </summary>
+    public virtual void Show()
+    {
+
+    }
+
+    /// <summary>
+    /// 隐藏自己
+    /// </summary>
+    public virtual void Hide()
+    {
+
+    }
+}
---
+++ UnityGameFrame/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum UILayer
+{
+    BOTTOM,
+    MIDDLE,
+    TOP,
+    SYSTEM
+}
+
+public class UIManager : BaseManager<UIManager> {
+
+    public Transform canvas;
+    public Transform bottom;
+    public Transform middle;
+    public Transform top;
+    public Transform system;
+
+    private Dictionary<string, BasePanel> panelDic = new Dictionary<string, BasePanel>();
+
+    public UIManager()
+    {
+        GameObject obj = ResManager.GetInstance().LoadRes<GameObject>("UI/Canvas");
+        canvas = obj.transform;
+        bottom = canvas.Find("Bottom").transform;
+        middle = canvas.Find("Middle").transform;
+        top = canvas.Find("Top").transform;
+        system = canvas.Find("System").transform;
+        obj = ResManager.GetInstance().LoadRes<GameObject>("UI/EventSystem");
+    }
+
+    /// <summary>
+    /// 通过层级枚举 得到对应层级的父对象
+    /// </summary>
+    /// <param name="layer"></param>
+    /// <returns></returns>
+    public Transform GetLayerFather(UILayer layer)
+    {
+        switch (layer)
+        {
+            case UILayer.BOTTOM:
+                return bottom;
+            case UILayer.MIDDLE:
+                return middle;
+            case UILayer.TOP:
+                return top;
+            case UILayer.SYSTEM:
+                return system;
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// 显示面板
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="name"></param>
+    /// <param name="layer"></param>
+    /// <param name="func"></param>
+    public void ShowPanel<T>(string name, UILayer layer,UnityAction<T> func = null) where T:BasePanel
+    {
+        if (panelDic.ContainsKey(name))
+        {
+            if (func != null)
+            {
+                func(panelDic[name] as T);
+            }
+            panelDic[name].Show();
+        }
+        else
+        {
+            ResManager.GetInstance().LoadResAsync<GameObject>("UI" + name, (obj) =>
+            {
+                Transform parent = GetLayerFather(layer);
+                obj.transform.SetParent(parent);
+                obj.transform.localScale = Vector3.one;
+                obj.transform.localPosition = Vector3.zero;
+                (obj.transform as RectTransform).offsetMax = Vector2.zero;
+                (obj.transform as RectTransform).offsetMin = Vector2.zero;
+
+                T panel = obj.GetComponent<T>();
+                if (func != null)
+                {
+                    func(panel);
+                }
+                panel.Show();
+                panelDic.Add(name, panel);
+            });
+        }
+    }
+
+    /// <summary>
+    /// 隐藏面板
+    /// </summary>
+    /// <param name="name"></param>
+    public void HidePanel(string name)
+    {
+        if (panelDic.ContainsKey(name))
+        {
+            panelDic[name].Hide();
+            GameObject.Destroy(panelDic[name].gameObject);
+            panelDic.Remove(name);
+        }
+    }
+
+    /// <summary>
+    /// 得到某一个已经显示的面板 方便外部使用
+    /// </summary>
+    public T GetPanel<T>(string name) where T : BasePanel
+    {
+        if (panelDic.ContainsKey(name))
+            return panelDic[name] as T;
+        return null;
+    }
+}
